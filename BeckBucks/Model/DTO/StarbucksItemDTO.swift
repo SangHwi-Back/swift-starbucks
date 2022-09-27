77e8258a7211b499e9a0406690518b2528d89073
백상휘 <sanghwiback@gmail.com>
2022-09-27 12:26:07 +0900

    Add starbucks item json and dots

---
+++ BeckBucks/Model/DTO/StarbucksItemDTO.swift
@@ -0,0 +1,13 @@
+//
+//  StarbucksItemDTO.swift
+//  BeckBucks
+//
+//  Created by 백상휘 on 2022/09/27.
+//
+
+import Foundation
+
+struct StarbucksItemDTO: Decodable {
+  let title: String
+  let name: String
+}
